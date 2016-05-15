6e3222624536a280e275e3b32fb02e7f7548a3e0
Lucas Pirola <lucaspirola@gmail.com>
2016-05-15 14:01:17 -0300

    Template limpo, com 3 slides em branco e 1 video

--- nike-version/skrollr/js/main.js
+++ nike-version/skrollr/js/main.js
@@ -28,40 +28,13 @@ function playVideo() {
 }
 $(document).ready(function() {
     $('body').css('visibility', 'visible');
-    var input = document.createElement("input");
-    if (('placeholder' in input) == false) {
-        $('[placeholder]').focus(function() {
-            var i = $(this);
-            if (i.val() == i.attr('placeholder')) {
-                i.val('').removeClass('placeholder');
-                if (i.hasClass('password')) {
-                    i.removeClass('password');
-                    this.type = 'password';
-                }
-            }
-        }).blur(function() {
-            var i = $(this);
-            if (i.val() == '' || i.val() == i.attr('placeholder')) {
-                if (this.type == 'password') {
-                    i.addClass('password');
-                    this.type = 'text';
-                }
-                i.addClass('placeholder').val(i.attr('placeholder'));
-            }
-        }).blur().parents('form').submit(function() {
-            $(this).find('[placeholder]').each(function() {
-                var i = $(this);
-                if (i.val() == i.attr('placeholder'))
-                    i.val('');
-            })
-        });
-    }
+
     var firstSection = 0;
     var lastSection = 4;
     var currentSection = 0;
     var scrollingBeHappening = false;
     var windowHeight = $(window).height();
-    
+
     $('.scroll-down-container').click(function() {
         animatedScrollTo("#content-container-1");
         currentSection = 1;
@@ -130,13 +103,7 @@ $(document).ready(function() {
             scrollTop: $(id).offset().top
         }, 1000);
     };
-    $('.language-dropdown').hide();
-    $('.language-container').mouseover(function(){
-        $('.language-dropdown').show();
-    });
-    $('.language-container').mouseout(function(){
-        $('.language-dropdown').hide();
-    });
+
     $('#tracker-node-0').click(function() {
         animatedScrollTo("#content-container-0");
         currentSection = 0;
